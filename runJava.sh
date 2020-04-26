a93ed92c0b9b6651b66d76992d8c827c28144e5c
sd1253 <sd1253@wildcats.unh.edu>
2020-04-26 11:15:04 -0400

    Update runJava.sh

---
+++ runJava.sh
@@ -0,0 +1,11 @@
+chmod +x script.sh
+git clone https://gitlab.cs.unh.edu/cs953-spring-2020/team-07-submission-2.git
+cd team07-submission03
+
+
+mvn clean install
+
+wget "http://www.cs.unh.edu/~dietz/download/got.cbor"
+
+java -jar target/project-1.0-SNAPSHOT-jar-with-dependencies.jar deaths-train.csv deaths-test.csv got.cbor Text/
+
