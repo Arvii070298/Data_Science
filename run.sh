02a0aa0743433af24bd9b4d8b558ba31c321a8b6
ab1545 <ab1545@wildcats.unh.edu>
2020-04-26 13:55:08 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 
-
 python Final_DT.py
 python Final_KNN.py
 python Final_BC.py
 python Final_XGB.py
 python Final_CustomML1.py
 python Final_CustomML2.py
-python Final_CustomML3.py
+#python Final_CustomML3.py
 
